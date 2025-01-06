4f825df79c426b4599e17e25422ec7c42950c7b5
Junia Paula <106549189+juniapaula230@users.noreply.github.com>
2025-01-06 10:09:49 -0300

    Add files via upload

---
+++ atividades/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividdes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void questão1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoje é quarta - feira!");
+        }
+
+        private void questão2(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.ShowDialog(); 
+        }
+
+        private void questão3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questão4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questão5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questão6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questão7(object sender, EventArgs e)
+        {
+
+        }
+
+        private void questão8(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ atividades/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividdes
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void num1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void escrevenum1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void escrevenum2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void escrevenum3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcular(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtNum1.Text);
+            double b = Convert.ToDouble(txtNum2.Text);
+            double c = Convert.ToDouble(txtNum3.Text);
+
+            double r = (a + b) * c;
+            MessageBox.Show("O resultado de (" + a+ "+" + b + ") "+" *" + c + " é: " + r.ToString());
+        }
+    }
+}
